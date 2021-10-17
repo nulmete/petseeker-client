f2265cc0d427554316ae02f695b615dedd251d78
nulmete <nicoulmete1@gmail.com>
2021-10-17 14:12:39 -0300

    setup MUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
 import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import theme from "./theme/theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
