e25f836d905859ade847a5cf19ef0dd897ea1624
nulmete <nicoulmete1@gmail.com>
2021-11-30 00:18:22 -0300

    change text input styles

--- src/components/Input/TextInput.tsx
+++ src/components/Input/TextInput.tsx
@@ -2,10 +2,27 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from "react";
 import { TextField, TextFieldProps } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const outlinedSelectors = [
+  "& .MuiOutlinedInput-notchedOutline",
+  "&:hover .MuiOutlinedInput-notchedOutline",
+];
+const CustomTextField = styled(TextField)(() => ({
+  "& label.Mui-focused": {
+    color: "black",
+  },
+  [outlinedSelectors.join(",")]: {
+    borderColor: "#adadad",
+  },
+  "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+    borderColor: "black",
+  },
+}));
 
 const TextInput: React.FC<TextFieldProps> = (props) => {
   return (
-    <TextField
+    <CustomTextField
       {...props}
       variant="outlined"
       fullWidth
